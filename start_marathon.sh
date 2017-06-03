49e8b47b8e8b557edd0bb64c51dcd759836f4a53
724686158 <solitarius.holic@gmail.com>
2017-06-03 13:15:25 +0800

    debug marathon

--- start_marathon.sh
+++ start_marathon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "please input you mesos-master's ipv4 address, and make ture zookeeper has opened"
 read ip
-nohup /opt/marathon-1.4.3/bin/start --master zk://$ip:2181/mesos --zk zk://$ip:2181/marathon --mesos_authentication --mesos_authentication_secret_file /tmp/mesos/config/marathon.secret --mesos_authentication_principal=marathon --mesos_role=marathon > /var/log/marathon/log 2>&1 
+nohup /opt/marathon-1.4.3/bin/start --master zk://localhost:2181/mesos --zk zk://localhost:2181/marathon > /var/log/marathon/log 2>&1 & 
