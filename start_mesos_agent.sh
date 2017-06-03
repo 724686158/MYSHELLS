c3585c5b9d89c47ed30750a708034debfe1f70fb
724686158 <solitarius.holic@gmail.com>
2017-06-03 11:22:58 +0800

    debug

--- start_mesos_agent.sh
+++ start_mesos_agent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "please input you mesos-master's ipv4 address"
 read ip
-nohup /opt/mesos-1.2.0/build/bin/mesos-agent.sh  --master=$ip:5050 --work_dir=/var/lib/mesos > /var/log/mesos/mesos-agent/log 2>&1 &
+nohup /opt/mesos-1.2.0/build/bin/mesos-agent.sh  --master=$ip:5050 --work_dir=/var/lib/mesos --containerizers=docker,mesos > /var/log/mesos/mesos-agent/log 2>&1 &
--- start_mesos_master.sh
+++ start_mesos_master.sh
@@ -2,4 +2,4 @@
 echo "be sure your zookeeper is started"
 echo "please input the master's ipv4 addr"
 read ip
-nohup /opt/mesos-1.2.0/build/bin/mesos-master.sh --ip=$ip --zk=zk://$ip:2181/mesos --work_dir=/var/lib/mesos --log_dir=/var/log/mesos  --quorum=1 > /var/log/mesos/mesos-master/log 2>&1 &
+nohup /opt/mesos-1.2.0/build/bin/mesos-master.sh --ip=$ip --zk=zk://$ip:2181/mesos --work_dir=/var/lib/mesos --log_dir=/var/log/mesos  --quorum=1 > /var/log/mesos/mesos-master/log.out 2>&1 &
