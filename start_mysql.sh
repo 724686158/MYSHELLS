98a357d99188993154e1436710187146d3d36b6c
724686158 <solitarius.holic@gmail.com>
2018-07-14 14:18:21 +0800

    mysql shell

--- start_mysql.sh
+++ start_mysql.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo "please input the mi_mysql's port"
+echo "please input the mysql's port"
 read port
-docker run --restart=always --name mysql$port -p $port:3306 -v /mysql/own/datadir/$port:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=mi -d daocloud.io/library/mysql:5.7
+echo "please input the root's password"
+read password
+docker run --restart=always --name mysql$port -p $port:3306 -v /mysql/own/datadir/$port:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$password -d daocloud.io/library/mysql:5.7
